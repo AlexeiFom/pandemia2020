6dd7b457f6716bd34a7c54205cf5aa5815fac94a
Alexei Fomin <1stan@DESKTOP-U84NEP9>
2020-04-13 20:21:29 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './shared/components/login/login.component';
+import { AboutComponent } from './shared/components/about/about.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { RegisterComponent } from './shared/components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { AboutComponent } from './shared/components/about/about.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { RegisterComponent } from './shared/components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
