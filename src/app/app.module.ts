bfe25fccdeb1576178bf2417d0a7ffa1b2cb9283
Алексей Алексей <1stanlyalex86@gmail.com>
2020-04-18 13:10:39 +0300

    add google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './shared/components/login/login.component';
@@ -9,6 +9,13 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { AboutComponent } from './shared/components/about/about.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { RegisterComponent } from './shared/components/register/register.component';
+import { environment } from 'src/environments/environment';
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+
+firebase.initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -22,7 +29,7 @@ import { RegisterComponent } from './shared/components/register/register.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule 
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+
+import { DashboardComponent } from '@app/modules/user/dashboard/dashboard.component';
+// import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  ui: firebaseui.auth.AuthUI;
+  constructor() {
+    this.ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+  }
 
   ngOnInit(): void {
+    debugger;
+
+    //let url = __dirname;
+
+    this.ui.start('#firebaseui-auth-container', {
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          return true;
+        },
+        uiShown: function () {
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      signInFlow: 'popup',
+      signInSuccessUrl: 'https://www.google.com',
+      // signInSuccessUrl:  __dirname 'https://www.google.com',
+
+      signInOptions: [
+        {
+          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        },
+        {
+          provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          scopes: [
+            'public_profile',
+            'email',
+            'user_likes',
+            'user_friends'
+          ],
+          customParameters: {
+            // Forces password re-entry.
+            auth_type: 'reauthenticate'
+          }
+        }
+      ],
+      // Other config options...
+    });
+    //}
+
   }
 
 }
